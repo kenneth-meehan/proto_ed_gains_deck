ae76d9bd57c442af83460e5cd8359499d16ebd3f
Kenneth Meehan <kenneth.meehan@hmhco.com>
2018-04-14 14:38:21 -0400

    add toggle for user to specify y-axis quantity: total items or items per student

--- app.R
+++ app.R
@@ -11,27 +11,30 @@ library(lubridate)
 library(scales)
 
 #Read data
-setwd("data")
-files.to.read <- list.files(pattern="*.zip$") #generate list of .zip filenames
-filesread <- data.frame(matrix(ncol=2, nrow=0))
-colnames(filesread) <- c("Input File Name", "Records Read")
-f <- 0  #index for dataframe to display input file names and record counts.
-for (zipfile in files.to.read){
-  f <- f+1
-  csvfile <- gsub(".zip$", "", zipfile)  #csv inside zip file has almost the same name as zip file.
-  df <- read.csv(unz(zipfile,csvfile), colClasses = "character")
-  filesread[f,1] <- zipfile
-  filesread[f,2] <- nrow(df)
-  if (!exists("dfall")){        #create dfall first time through
-    dfall <- read.table(text = "", #create a blank dfall dataframe with the proper columns
-                        colClasses = "character",
-                        col.names = colnames(df))
-  }
-  dfall <- rbind(dfall, df) #add current df to the bottom of dfall
-}
-df <- dfall
-rm(dfall)
-setwd("..")
+# setwd("data")
+# files.to.read <- list.files(pattern="*.zip$") #generate list of .zip filenames
+# filesread <- data.frame(matrix(ncol=2, nrow=0))
+# colnames(filesread) <- c("Input File Name", "Records Read")
+# f <- 0  #index for dataframe to display input file names and record counts.
+# for (zipfile in files.to.read){
+#   f <- f+1
+#   csvfile <- gsub(".zip$", "", zipfile)  #csv inside zip file has almost the same name as zip file.
+#   df <- read.csv(unz(zipfile,csvfile), colClasses = "character")
+#   filesread[f,1] <- zipfile
+#   filesread[f,2] <- nrow(df)
+#   if (!exists("dfall")){        #create dfall first time through
+#     dfall <- read.table(text = "", #create a blank dfall dataframe with the proper columns
+#                         colClasses = "character",
+#                         col.names = colnames(df))
+#   }
+#   dfall <- rbind(dfall, df) #add current df to the bottom of dfall
+# }
+# df <- dfall
+# rm(dfall)
+# setwd("..")
+
+df <- read.csv("BrowardEd.csv", colClasses = "character")   #for testing purposes
+
 
 #Set data types properly
 df$mo_yr_completed <- as.Date(floor_date(ymd_hms(df$date_completed),"month"))
@@ -41,7 +44,6 @@ df$time_in_secs <- as.integer(df$time_in_secs)
 df <- df[nchar(df$grade)>0,]  #exclude records with grade of ""
 df <- df[!is.na(df$grade),]   #exclude records without grade
 df$grade <- paste("Grade", df$grade)
-
 
 
 # Define UI for application that draws a facted graph
@@ -60,7 +62,10 @@ ui <- fluidPage(
         #checkboxInput("scoresorno", "Show average scores?", FALSE)
         selectInput(inputId = "extraStats", label = strong("Show Additional Stats?"),
                     choices = c("Number of Students", "Average Scores", "Average Durations in Seconds", "None"),
-                    selected = "None")
+                    selected = "Number of Students"),
+        radioButtons("yaxis", "Vertical Axis Represents What?",
+                     c("Number of Items (total)" = "NumberOfItems",
+                       "Items per (active) Student" = "ItemsPerStudent"))
       ),
       
       # Show a faceted graph
@@ -81,8 +86,9 @@ server <- function(input, output) {
        
           BySchoolMonth <- dist %>%
             group_by(school_name, mo_yr_completed) %>%
-            summarize(nItems=n(),
+            summarize(NumberOfItems=n(),
                       nStudents=n_distinct(student_personal_refid),
+                      ItemsPerStudent=NumberOfItems/nStudents,
                       avgScore=sum(item_score)/sum(item_max_score),
                       avgDur=mean(time_in_secs))
           
@@ -105,13 +111,14 @@ server <- function(input, output) {
               BySchoolMonth$avgScore <- NA
             }
 
-          ggplot(BySchoolMonth, aes(x=mo_yr_completed, y=nItems)) +
+          # ggplot(BySchoolMonth, aes(x=mo_yr_completed, y=nItems)) +
+          ggplot(BySchoolMonth, aes(x=mo_yr_completed, y=eval(as.name(input$yaxis)))) +
             geom_bar(stat="identity", fill='goldenrod') +
             scale_x_date(date_breaks = "1 month", date_labels = "%b %Y") +
             scale_y_continuous(labels=comma) +
             labs(title="Ed Learnosity Items by School and Month",
                  subtitle=paste("Blue Annotations:",input$extraStats),
-                 x="", y="Number of Items") +
+                 x="", y=as.name(input$yaxis)) +
             facet_grid(school_name ~ .) +
             theme(strip.text.y = element_text(angle=0),
                   legend.position="none") + 
@@ -129,8 +136,9 @@ server <- function(input, output) {
          
           BySchoolGradeMonth <- dist %>%
             group_by(school_name, grade, mo_yr_completed) %>%
-            summarize(nItems=n(),
+            summarize(NumberOfItems=n(),
                       nStudents=n_distinct(student_personal_refid),
+                      ItemsPerStudent=NumberOfItems/nStudents,
                       avgScore=sum(item_score)/sum(item_max_score),
                       avgDur=mean(time_in_secs))
           #Eliminate display of stats as user specifies
@@ -152,12 +160,12 @@ server <- function(input, output) {
             BySchoolGradeMonth$avgScore <- NA
           }
           
-          ggplot(BySchoolGradeMonth, aes(x=grade, y=nItems)) +
+          ggplot(BySchoolGradeMonth, aes(x=grade, y=eval(as.name(input$yaxis)))) +
               geom_bar(stat="identity", fill='goldenrod') +
               scale_y_continuous(labels=comma) +
               labs(title="Ed Learnosity Items by School, Month, and Grade",
                    subtitle=paste("Blue Annotations:",input$extraStats),
-                   x="", y="Number of Items") +
+                   x="", y=as.name(input$yaxis)) +
               facet_grid(school_name ~ as.factor(substr(mo_yr_completed,1,7))) +
               theme(strip.text.y = element_text(angle=0),
                     axis.text.x = element_text(angle=90, vjust=0.5),
