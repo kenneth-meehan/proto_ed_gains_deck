23d06414cddd95914a09a434d001a717f364ce6c
Kenneth Meehan <kenneth.meehan@hmhco.com>
2018-04-18 10:08:58 -0400

    add marginals to coarser graph of items-per-student faceted graph

--- app.R
+++ app.R
@@ -62,7 +62,11 @@ server <- function(input, output) {
      output$distPlot <- renderPlot({
        
        dist <- df %>% filter(district_name==input$district)
-       mindate <- min(dist$mo_yr_completed)
+       
+       #Get, as date and as string, the first month BEFORE any activity in the data.
+       #Use this as placeholder for ALL category, to hold stats for each school (and district) over all time.
+       mindateless1mo <- min(dist$mo_yr_completed) - months(1)
+       mindateless1mostring <- substr(as.character(mindateless1mo),1,7)
 
        if(!input$gradesorno){
        
@@ -82,7 +86,7 @@ server <- function(input, output) {
              #make marginals by school
              BySchool <- BySchoolMonth %>%
                group_by(school_name) %>%
-               summarize(mo_yr_completed=mindate-months(1),   #Hope we can change label on x-axis from Aug 17 to ALL
+               summarize(mo_yr_completed=mindateless1mo,   #Hope we can change label on x-axis from Aug 17 to ALL
                          NumberOfItems=sum(NumberOfItems),
                          NumbersOfStudents=sum(NumbersOfStudents),
                          ItemsPerStudent=round(NumberOfItems/NumbersOfStudents),
@@ -95,7 +99,7 @@ server <- function(input, output) {
              
              ByMonth <- BySchoolMonth %>%
                group_by(mo_yr_completed) %>%
-               summarize(school_name="ALL",
+               summarize(school_name="ZZZ",    #assuming that ZZZ will be later alphabetically than any school name
                          NumberOfItems=sum(NumberOfItems),
                          NumbersOfStudents=sum(NumbersOfStudents),
                          ItemsPerStudent=round(NumberOfItems/NumbersOfStudents),
@@ -109,8 +113,8 @@ server <- function(input, output) {
              ByMonth <- ByMonth[c(2,1,3,4,5,6,7,8,9,10,11)] #order columns to match those of BySchoolMonth
              
              Overall <- ByMonth %>%
-               summarize(mo_yr_completed=mindate-months(1),   #Hope we can change label on x-axis from Aug 17 to ALL
-                         school_name="ALL",
+               summarize(mo_yr_completed=mindateless1mo, #temporarily put stats into earliest month - 1 month category
+                         school_name="ZZZ",
                          NumberOfItems=sum(NumberOfItems),
                          NumbersOfStudents=sum(NumbersOfStudents),
                          ItemsPerStudent=round(NumberOfItems/NumbersOfStudents),
@@ -144,10 +148,26 @@ server <- function(input, output) {
               BySchoolMonth$NumbersOfStudents <- NA
               BySchoolMonth$AvgScores <- NA
             }
-
+          
+          #Change labels for Per-School marginals (stats over ALL time) from Aug 2017 to ALL:
+          BySchoolMonth$mo_yr_completed <- as.factor(BySchoolMonth$mo_yr_completed)
+          BySchoolMonth$mo_yr_completed <- substr(as.character(BySchoolMonth$mo_yr_completed),1,7)
+          BySchoolMonth$mo_yr_completed[BySchoolMonth$mo_yr_completed==mindateless1mostring] <- "ALL"
+          
+          #Change labels for school_names so that ALL is displayed last
+          BySchoolMonth$school_name <- as.factor(BySchoolMonth$school_name)
+          BySchoolMonth$school_name <- as.character(BySchoolMonth$school_name)
+          BySchoolMonth <- arrange(BySchoolMonth, school_name)   #to ensure that ZZZ comes last
+          BySchoolMonth$school_name <- as.factor(BySchoolMonth$school_name)
+          levels(BySchoolMonth$school_name)[levels(BySchoolMonth$school_name)=="ZZZ"] <- "ALL"
+          #BySchoolMonth$school_name[BySchoolMonth$school_name=="ZZZ"] <- "ALL"
+          
+                    
+          
+          
           ggplot(BySchoolMonth, aes(x=mo_yr_completed, y=eval(as.name(input$yaxis)))) +
             geom_bar(stat="identity", fill='goldenrod') +
-            scale_x_date(date_breaks = "1 month", date_labels = "%b %Y") +
+            #scale_x_date(date_breaks = "1 month", date_labels = "%b %Y") +
             scale_y_continuous(labels=comma) +
             labs(title="Ed Learnosity Items by School and Month",
                  subtitle=paste("Blue Annotations:",input$extraStats),
